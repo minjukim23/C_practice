c65474626383adf3bc7e471210dda07259b044c9
minju kim <rlaalswn1229@naver.com>
2023-08-12 22:42:57 +0900

    first commit

---
+++ 2480.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int a,b,c;
+    int max;
+    scanf("%d %d %d",&a,&b,&c);
+    if (a==b && b==c && a==c) {
+        printf("%d",10000+a*1000);
+    }
+    else if (a==b && b!=c) {
+        printf("%d",1000+a*100);
+    }
+    else if (b==c && a!=b) {
+        printf("%d",1000+b*100);
+    }
+    else if (a==c && a!=b) {
+        printf("%d",1000+a*100);
+    }
+    else {
+        if(a>b && a>c) {
+            max = a;
+        }
+        else if (b>c && b>a) {
+            max = b;
+        }
+        else if (c>a && c>b) {
+            max = c;
+        }
+        printf("%d",max*100);
+    }
+      
+    return 0;
+}
